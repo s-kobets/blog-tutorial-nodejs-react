6e7a1b55168515592b468ec5b2e7cb6ae0a59a90
Khotey <vitaliykhotey@Khoteys-MacBook-Pro.local>
2016-10-01 15:29:55 +0300

    lesson-2

---
+++ controllers/auth.js
@@ -0,0 +1,42 @@
+import User from '../models/user';
+
+export const signup = async (req, res, next) => {
+  const credentials = req.body;
+  let user;
+
+  try {
+    user = await User.create(credentials);
+  } catch ({ message }) {
+    return next({
+      status: 400,
+      message
+    });
+  }
+
+  res.json(user);
+}
+
+export const signin = async (req, res, next) => {
+  const { login, password } = req.body;
+
+  const user = await User.findOne({ login });
+
+  if (!user) {
+    return next({
+      status: 400,
+      message: 'User not found'
+    });
+  }
+
+  try {
+    const result = await user.comparePasswords(password);
+  } catch (e) {
+    return next({
+      status: 400,
+      message: 'Bad Credentials'
+    });
+  }
+
+  req.session.userId = user._id;
+  res.json(user);
+}
---
+++ middlewares/errorHandler.js
@@ -0,0 +1,7 @@
+export default function(err, req, res, next) {
+  let { status = 500, message = 'Server Error' } = err;
+
+  return res
+    .status(status)
+    .json({ message });
+};
