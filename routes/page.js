601a02b609d2baafc5f5fa26413b23cec4729aa9
Khotey <vitaliykhotey@Khoteys-MacBook-Pro.local>
2016-11-09 16:00:44 +0200

    lesson-6: get pages by user login, delete user page

--- routes/page.js
+++ routes/page.js
@@ -6,5 +6,7 @@ const router = express.Router();
 
 router.post('/pages', PageController.create);
 router.get('/pages', PageController.getAll);
+router.get('/pages/:login', PageController.getPagesByUserLogin);
+router.delete('/pages/:id', PageController.deletePage);
 
 export default router;
